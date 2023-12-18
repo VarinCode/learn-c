2018e2bec4d23dfd51433bbba17dc597d24f23e8
VarinCode <warinsaipanya471@outlook.co.th>
2023-12-18 15:05:42 +0700

    Enable loop to print numbers in increment of 2

---
+++ week4/main1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int i, j = 8;
+    // for (i = 1; i <=5; i++){
+    //     printf("%d %d \n",i,j );
+    //     j -= 2;
+    // }
+    for (i = 1; i <=5; i+=2){
+    printf("%d %d \n",i,j );
+    j -= 2;
+    }
+    return 0;
+}
